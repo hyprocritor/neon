39798410e62eab17891d158381b6707428608bee
Hyprocritor <i@xsx.st>
2024-11-06 22:00:22 +0800

    fix styles and update dev containers
    Signed-off-by: Hyprocritor <i@xsx.st>

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,5 @@
 export const SITE_TITLE = '在 23 时 ';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 
-export const AVATAR_LINK = ""
-export const AUTHOR_NAME = ""
+export const AVATAR_LINK = "https://weavatar.com/avatar/528141c582f9a6d590b55a558453467b?size=512"
+export const AUTHOR_NAME = "Hyprocritor"
